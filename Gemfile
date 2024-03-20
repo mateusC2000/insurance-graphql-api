db57648b70834836c289c7a9aea35b8b543de668
mateusC2000 <mateus.camphos2000@gmail.com>
2024-03-20 16:39:11 -0300

    Running rubocop

--- Gemfile
+++ Gemfile
@@ -1,16 +1,20 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.3.0"
-gem "rails", "~> 7.0.8", ">= 7.0.8.1"
+ruby '3.3.0'
+gem 'rails', '~> 7.0.8', '>= 7.0.8.1'
 
-gem "sqlite3", "~> 1.4"
-gem "puma", "~> 5.0"
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
-gem "bootsnap", require: false
-gem "graphql", "~> 2.0"
-gem "graphiql-rails", group: :development
+gem 'bootsnap', require: false
+gem 'graphiql-rails', group: :development
+gem 'graphql', '~> 2.0'
+gem 'puma', '~> 5.0'
+gem 'sneakers'
+gem 'sqlite3', '~> 1.4'
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 group :development, :test do
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rubocop-rails', require: false
 end
--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -14,10 +14,12 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = InsuranceGraphqlApiSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = InsuranceGraphqlApiSchema.execute(query, variables:, context:,
+                                                      operation_name:)
     render json: result
   rescue StandardError => e
     raise e unless Rails.env.development?
+
     handle_error_in_development(e)
   end
 
@@ -43,10 +45,11 @@ class GraphqlController < ApplicationController
     end
   end
 
-  def handle_error_in_development(e)
-    logger.error e.message
-    logger.error e.backtrace.join("\n")
+  def handle_error_in_development(error)
+    logger.error error.message
+    logger.error error.backtrace.join("\n")
 
-    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
+    render json: { errors: [{ message: error.message, backtrace: error.backtrace }], data: {} },
+           status: :internal_server_error
   end
 end
