1f2301747a51e505b4894999c507c0fcab68338b
mateusC2000 <mateus.camphos2000@gmail.com>
2024-03-21 15:34:25 -0300

    Running rubocop.

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,19 +1,11 @@
 # frozen_string_literal: true
 
 class GraphqlController < ApplicationController
-  # If accessing from outside this domain, nullify the session
-  # This allows for outside API access while preventing CSRF attacks,
-  # but you'll have to authenticate your user separately
-  # protect_from_forgery with: :null_session
-
   def execute
     variables = prepare_variables(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
-    context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
-    }
+    context = {}
     result = InsuranceGraphqlApiSchema.execute(query, variables:, context:,
                                                       operation_name:)
     render json: result
@@ -25,23 +17,25 @@ class GraphqlController < ApplicationController
 
   private
 
-  # Handle variables in form data, JSON body, or a blank value
   def prepare_variables(variables_param)
+    return method_aleat(variables_param) if variables_param.instance_of?(String)
+    return variables_param if variables_param.instance_of?(Hash)
+
     case variables_param
-    when String
-      if variables_param.present?
-        JSON.parse(variables_param) || {}
-      else
-        {}
-      end
-    when Hash
-      variables_param
     when ActionController::Parameters
-      variables_param.to_unsafe_hash # GraphQL-Ruby will validate name and type of incoming variables.
+      variables_param.to_unsafe_hash
     when nil
       {}
     else
       raise ArgumentError, "Unexpected parameter: #{variables_param}"
+    end
+  end
+
+  def method_aleat(variables_param)
+    if variables_param.present?
+      JSON.parse(variables_param) || {}
+    else
+      {}
     end
   end
 
