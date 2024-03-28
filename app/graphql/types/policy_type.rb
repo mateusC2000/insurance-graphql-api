d544a03e443c7c3f5db3e8461ea4131a9006e23b
mateusC2000 <mateus.camphos2000@gmail.com>
2024-03-28 10:50:22 -0300

    Add 'null:false' in Types::PolicyType fields.

--- app/graphql/types/policy_type.rb
+++ app/graphql/types/policy_type.rb
@@ -2,10 +2,10 @@
 
 module Types
   class PolicyType < ::Types::BaseObject
-    field :number, String
-    field :emission_date, String
-    field :coverage_end_date, String
-    field :insured, Types::InsuredType
-    field :vehicle, Types::VehicleType
+    field :number, String, null: false
+    field :emission_date, String, null: false
+    field :coverage_end_date, String, null: false
+    field :insured, Types::InsuredType, null: false
+    field :vehicle, Types::VehicleType, null: false
   end
 end
